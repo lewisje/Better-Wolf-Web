d78f560a54564513ba211cf9b6abb27b34705b51
James Edward Lewis II <lewisje@alumni.iu.edu>
2014-07-27 21:38:47 -0400

    Update bww.commands.js

--- bww.commands.js
+++ bww.commands.js
@@ -4,34 +4,36 @@
 GM_registerMenuCommand("Clear blocked users list", GM_deleteValue("blocked_users"));
 
 GM_registerMenuCommand("Set watched keywords",
-	function() {
-		current_keywords = GM_getValue("keywords", "nsfw");
-		new_keywords = prompt("Enter a comma-separated list of keywords to watch for and highlight.", current_keywords);
+  function() {
+    'use strict';
+    var current_keywords = GM_getValue("keywords", "nsfw");
+    var new_keywords = prompt("Enter a comma-separated list of keywords to watch for and highlight.", current_keywords);
 
-		new_keywords = new_keywords.split(",");
+    new_keywords = new_keywords.split(",");
 
-		new_keywords.forEach(function (element, index, array) {
-			array[index] = element.trim();
-		});
-		new_keywords = new_keywords.sort();
-		new_keywords = new_keywords.join(", ");
+    new_keywords.forEach(function (element, index, array) {
+      array[index] = element.trim();
+    });
+    new_keywords = new_keywords.sort();
+    new_keywords = new_keywords.join(", ");
 
-		if (debugMode) {
-			console.log("Keywords: " + new_keywords);
-		}
-		GM_setValue("keywords", new_keywords);
-	});
+    if (debugMode) {
+      console.log("Keywords: " + new_keywords);
+    }
+    GM_setValue("keywords", new_keywords);
+  });
 
 GM_registerMenuCommand("Toggle debug mode",
-	function() {
-		debugMode = GM_getValue("debug_mode", false);
-		debugMode = !debugMode;
-		GM_setValue("debug_mode", debugMode);
+  function() {
+    'use strict';
+    var debugMode = GM_getValue("debug_mode", false);
+    debugMode = !debugMode;
+    GM_setValue("debug_mode", debugMode);
 
-		if (debugMode) {
-			alert("Debug mode on");
-		} else {
-			alert("Debug mode off");
-		}
+    if (debugMode) {
+      alert("Debug mode on");
+    } else {
+      alert("Debug mode off");
+    }
 
-	});
+  });

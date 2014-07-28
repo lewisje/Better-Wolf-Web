08dcc4793f2e0c1fc309051969df819c20ca1017
James Edward Lewis II <lewisje@alumni.iu.edu>
2014-07-27 23:18:13 -0400

    Update better_wolf_web.user.js

--- better_wolf_web.user.js
+++ better_wolf_web.user.js
@@ -1,26 +1,28 @@
 // ==UserScript==
-// @name          Better Wolf Web
-// @author        Jonathan Hamilton - Modified for Chrome by Mark Evaul - Enhanced by James Lewis
-// @namespace     http://jlhamilt.freeshell.org/
-// @version       2.0
-// @description   Extensions for the Wolf Web
-// @include       http://*.thewolfweb.com/*
-// @include       https://*.thewolfweb.com/*
-// @include       http://*.brentroad.com/*
-// @include       https://*.brentroad.com/*
-// @exclude       http://site3.thewolfweb.com/*
-// @exclude       https://site3.thewolfweb.com/*
-// @require       https://raw.githubusercontent.com/douglascrockford/JSON-js/master/json2.js
-// @require       https://raw.githubusercontent.com/douglascrockford/JSON-js/master/cycle.js
-// @require       https://code.jquery.com/jquery-1.11.1.min.js
-// @require       https://code.jquery.com/jquery-migrate-1.2.1.min.js
-// @require       https://raw.githubusercontent.com/lewisje/Better-Wolf-Web/master/bww.utils.js
-// @require       https://raw.githubusercontent.com/lewisje/Better-Wolf-Web/master/bww.controls.js
-// @require       https://raw.githubusercontent.com/lewisje/Better-Wolf-Web/master/bww.media.js
-// @require       https://raw.githubusercontent.com/lewisje/Better-Wolf-Web/master/bww.commands.js
-// @require       https://raw.githubusercontent.com/lewisje/Better-Wolf-Web/master/bww.blocking.js
-// @require       https://raw.githubusercontent.com/lewisje/Better-Wolf-Web/master/scaffold.js
-// @require       https://raw.githubusercontent.com/lewisje/Better-Wolf-Web/master/gm_jq_xhr.js
+// @name        Better Wolf Web
+// @author      Jonathan Hamilton
+// @contributor Mark Evaul
+// @contributor James Lewis
+// @namespace   http://jlhamilt.freeshell.org/
+// @icon        http://thewolfweb.com/favicon.ico
+// @version     2.0
+// @description Extensions for the Wolf Web
+// @match       *://*.thewolfweb.com/*
+// @match       *://*.brentroad.com/*
+// @exclude     http://site3.thewolfweb.com/*
+// @exclude     https://site3.thewolfweb.com/*
+// @require     https://raw.githubusercontent.com/douglascrockford/JSON-js/master/json2.js
+// @require     https://code.jquery.com/jquery-1.11.1.min.js
+// @require     https://code.jquery.com/jquery-migrate-1.2.1.min.js
+// @require     https://raw.githubusercontent.com/lewisje/Better-Wolf-Web/master/compat.js
+// @require     https://raw.githubusercontent.com/lewisje/Better-Wolf-Web/master/bww.utils.js
+// @require     https://raw.githubusercontent.com/lewisje/Better-Wolf-Web/master/bww.controls.js
+// @require     https://raw.githubusercontent.com/lewisje/Better-Wolf-Web/master/bww.media.js
+// @require     https://raw.githubusercontent.com/lewisje/Better-Wolf-Web/master/bww.commands.js
+// @require     https://raw.githubusercontent.com/lewisje/Better-Wolf-Web/master/bww.blocking.js
+// @require     https://raw.githubusercontent.com/lewisje/Better-Wolf-Web/master/scaffold.js
+// @require     https://raw.githubusercontent.com/lewisje/Better-Wolf-Web/master/gm_jq_xhr.js
+// @run-at      document-end
 // ==/UserScript==
 
 if (window.top !== window.self) {

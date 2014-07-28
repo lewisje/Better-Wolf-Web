1025f40282a6bb08b70fd8fe9d821c8fc99e0a7d
lewisje <lewisje@alumni.iu.edu>
2014-07-27 20:11:01 -0400

    Update better_wolf_web.user.js

--- better_wolf_web.user.js
+++ better_wolf_web.user.js
@@ -1,24 +1,27 @@
 // ==UserScript==
 // @name          Better Wolf Web
-// @author        Jonathan Hamilton
+// @author        Jonathan Hamilton - Modified for Chrome by Mark Evaul - Enhanced by James Lewis
 // @namespace     http://jlhamilt.freeshell.org/
 // @version       2.0
 // @description   Extensions for the Wolf Web
-// @include       http://*thewolfweb.com/*
-// @include       http://*brentroad.com/*
+// @include       http://*.thewolfweb.com/*
+// @include       http://*.brentroad.com/*
 // @exclude       http://site3.thewolfweb.com/*
-// @require       http://jqueryjs.googlecode.com/files/jquery-1.3.2.min.js
-// @require       https://raw.github.com/FroshKiller/Better-Wolf-Web/master/bww.utils.js
-// @require       https://raw.github.com/FroshKiller/Better-Wolf-Web/master/bww.commands.js
-// @require       https://raw.github.com/FroshKiller/Better-Wolf-Web/master/bww.blocking.js
-// @require       https://raw.github.com/FroshKiller/Better-Wolf-Web/master/scaffold.js
-// @require       https://raw.github.com/FroshKiller/Better-Wolf-Web/master/gm_jq_xhr.js
+// @require       https://code.jquery.com/jquery-1.11.1.min.js
+// @require       https://code.jquery.com/jquery-migrate-1.2.1.min.js
+// @require       https://raw.github.com/lewisje/Better-Wolf-Web/master/bww.utils.js
+// @require       https://raw.github.com/lewisje/Better-Wolf-Web/master/bww.commands.js
+// @require       https://raw.github.com/lewisje/Better-Wolf-Web/master/bww.blocking.js
+// @require       https://raw.github.com/lewisje/Better-Wolf-Web/master/scaffold.js
+// @require       https://raw.github.com/lewisje/Better-Wolf-Web/master/gm_jq_xhr.js
 // ==/UserScript==
 
-if (window.top != window.self) {
-	$.noop(); // Don't run the script if the page is in an IFRAME.
-} else {
-	debugMode = GM_getValue("debug_mode", false);
+;(function () {
+  'use strict';
+  if (window.top !== window.self) {
+    $.noop(); // Don't run the script if the page is in an IFRAME.
+  } else {
+    debugMode = GM_getValue("debug_mode", false);
 
 	if (debugMode) {
 		console.group("Better Wolf Web");
@@ -35,7 +38,8 @@ if (window.top != window.self) {
 			threadList = scaffoldThreads();
 			break;
 		case "/message_topic.aspx":
-			[postsInThread, usersInThread] = scaffoldThread(); // Firefox supports multiple returns.
+			postsInThread = scaffoldThread()[0]; // Firefox supports multiple returns.
+			usersInthread = scaffoldThread()[1];
 			break;
 		case "/user_info.aspx":
 			currentUser = scaffoldUserProfile();
@@ -52,3 +56,4 @@ if (window.top != window.self) {
 		console.groupEnd("Better Wolf Web");
 	}
 }
+}());
